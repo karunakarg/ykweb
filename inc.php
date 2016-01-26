65d4000227fa27ee1a1975430fd1f51c267eeb74
karunakarg <karunakar.gautam@gmail.com>
2016-01-27 01:30:57 +0530

    added material design css

--- inc.php
+++ inc.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['ip'])) { // if new user
 <link rel="icon" type="image/png" href="favicon.png">
 <meta name="generator" content="YourKart" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        <!-- <link href="css/yourkart.min.css" rel="stylesheet"> -->
+        <link href="css/yourkart.min.css" rel="stylesheet">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.5.7/css/bootstrap-material-design.min.css" rel="stylesheet">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.5.7/css/ripples.min.css" rel="stylesheet">
         <link href="" rel="stylesheet">
